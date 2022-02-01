1a78963da0ddf1f6a67bfd96fda81ac1158bf4fc
sachins32 <sachinph32@gmail.com>
2022-02-01 18:59:28 +0530

    hello_world

---
+++ Java/hello_world.java
@@ -0,0 +1,11 @@
+class Hello_World {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+/* 
+Running Java Programs
+javac hello_world.java 
+java Hello_World 
+*/
