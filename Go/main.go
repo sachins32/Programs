175ddcaedb402b005e21ef29b04cf8a691acc59a
sachins32 <sachinph32@gmail.com>
2022-02-02 19:11:07 +0530

    hello_world

---
+++ Go/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+/*
+Creating a Go module
+go mod init module_name
+*/
+
+/*
+It's a convention in Go to name the file that contains
+the main function as main.go, but other names work as well.
+*/
+
+/*
+Running Go Programs
+go build
+./name_of_module
+*/
